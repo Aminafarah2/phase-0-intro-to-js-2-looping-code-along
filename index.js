2fc3491c034ab9e6ff31fd9ab6a5d7551a3d8ddb
aminafarah <amishkamohamed563@gmail.com>
2023-09-23 15:19:39 +0300

    passed test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const event = "celebration";
+
+function writeCards(names, celebration) {
+  const congratulatoryMessage = [];
+  for (let i = 0; i < names.length; i++) {
+    congratulatoryMessage.push(`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+  }
+  // Return the array of congratulatory messages after the loop
+  return congratulatoryMessage;
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
